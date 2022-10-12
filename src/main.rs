ecd851b9f3f093c4971b239f7a8e5953abdc7656
Dariusz Depta <dariusz.depta@dmntk.io>
2022-10-12 09:02:31 +0200

    Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use crate::plane::Plane;
+use std::fs;
+
+mod plane;
+
 fn main() {
-    println!("Hello, world!");
+  let file_name = "./examples/e1.dtb";
+  let content = fs::read_to_string(file_name).unwrap();
+  let plane = Plane::new(content);
+  println!("{}", plane);
 }
